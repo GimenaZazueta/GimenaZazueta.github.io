0e92153a57b268477ee7f0585280af45575b1625
GimenaZazueta <159394793+GimenaZazueta@users.noreply.github.com>
2024-09-13 19:23:06 -0700

    13/09/2024

---
+++ BaseDeDatosWeb/parcial1/practica1.php
@@ -0,0 +1,28 @@
+<?php
+
+    $nombre = $_GET["nombre"];
+    $edad = $_GET["edad"];
+    $escuela = $_GET["escuela"];
+    $juego = $_GET["juego"];
+    $comida = $_GET["comida"];
+    //obtienen los valores
+
+    if($edad > 18){
+        echo "<h1>".$nombre." es mayor de edad</h1>";
+    } else {
+        echo "<h1>".$nombre." NO es mayor de edad</h1>";
+    }
+    
+    echo "<h1>Nombre del Alumno: ".$nombre."</h1>";
+
+    echo "<h2>Edad: ".$edad."</h2>";
+
+    echo "<h2>Escuela: ".$escuela."</h2>";
+
+    echo "<h2>Juego favorito: ".$juego."</h2>";
+
+    echo "<h2>Comida favorita: ".$comida."</h2>";
+
+    //Imprimir el valor de esos campos
+
+?>
